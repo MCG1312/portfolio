6eff24632fe92c8ce307aad8739ac13e29c239d1
MCG1312 <aumssadm7@gmail.com>
2026-01-03 20:44:55 +0100

    Initial commit: Vite + Three.js Portfolio

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -6,7 +6,7 @@ import { createClient } from '@supabase/supabase-js';
 // 3. Copy your "Project URL" and "anon public" key.
 // 4. Paste them below.
 
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co'; 
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
+const SUPABASE_URL = 'https://uccqnyhfaozwjvwghjgv.supabase.co'; 
+const SUPABASE_ANON_KEY = 'sb_publishable_zXPkn22QdHczBAeql3U_zA_iIM-wuHk';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
